2b7395196166dbf7e186cfc5e2b231700219bb1c
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-07-11 01:43:48 -0400

    added chalkboard to resemble the one in the show

--- api.js
+++ api.js
@@ -4,21 +4,30 @@ fetch("https://bobsburgers-api.herokuapp.com/v2/burgerOfTheDay/")
 console.log(JSONresponse)})
 .catch(error => console.log(error));
 
-const burgerContainer = document.getElementById('burger-container');
+const burgerContainer = document.getElementById("burger-container");
 
 fetch("https://bobsburgers-api.herokuapp.com/v2/burgerOfTheDay/1")
   .then(response => response.json())
   .then(data => {
-    const burgerName = document.createElement('h2');
+    const burgerName = document.createElement("h2");
     burgerName.textContent = data.name;
 
-    const burgerPrice = document.createElement('p');
+    const burgerChalk = document.createElement("p")
+    burgerChalk.textContent = "BURGER"
+    burgerChalk.style.textDecoration = "underline"
+
+    const ofTheDayChalk = document.createElement("p")
+    ofTheDayChalk.textContent = "Of The Day"
+
+    const burgerPrice = document.createElement("p");
     burgerPrice.textContent = data.price;
 
+    burgerContainer.appendChild(burgerChalk);
+    burgerContainer.appendChild(ofTheDayChalk);
     burgerContainer.appendChild(burgerName);
     burgerContainer.appendChild(burgerPrice);
   })
   .catch(error => {
-    console.error('Error:', error);
-    burgerContainer.textContent = 'Failed to fetch the Burger of the Day. Please try again later.';
+    console.error("Error:", error);
+    burgerContainer.textContent = "Failed to fetch the Burger of the Day. Please try again later.";
   });
